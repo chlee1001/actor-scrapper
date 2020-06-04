55e8219cee2a3fa17cb24c1e1efbb2d78246c1b7
eurohand <eurohand@naver.com>
2020-06-05 01:05:32 +0900

    Details

--- keyword_scrapper/naver.py
+++ keyword_scrapper/naver.py
@@ -34,48 +34,47 @@ def cropFace(filenames):
         if (faces is None):
             print('Failed to detect face')
             continue
-        print(faces)
+        print(f"Face Detected: {faces}")
         for (x, y, w, h) in faces:
             cropped = gray[y:y + h, x:x + w]
             # 이미지를 저장
             cv2.imwrite("crop" + str(imgNum) + ".jpg", cropped)
-            print("Cropped!")
+            print(f"[crop{str(imgNum)}]: Cropped!")
             imgNum += 1
 
 
 def main():
-    # 사용한 네이버 url https://search.naver.com/search.naver?where=image&sm=tab_jum&query=%EC%97%AC%EC%9A%B0%EC%83%81
+    # 네이버 url https://search.naver.com/search.naver?where=image&sm=tab_jum&query=%EC%97%AC%EC%9A%B0%EC%83%81
     url_info = "https://search.naver.com/search.naver?"
-    # params에 딕션을 넣어줌
     params = {
-        # 명령행에서 받은 인자값을 people로 넣어줌
         "query": people,
         "where": "image",
         "face": 1
     }
-    # url 요청 파싱값
-    html_object = req.get(url_info, params)  # html_object html source 값
+    html_object = req.get(url_info, params)
 
     if html_object.status_code == 200:
-        # 페이지 status_code 가 200 일때 2XX 는 성공을 이야기함
+
         bs_object = BeautifulSoup(html_object.text, "html.parser")
-        # 인스턴스 생성
         img_data = bs_object.find_all("img", {"class": "_img"})
-        # 인스턴스의 find_all 이라는 함수에 img 태그가 있으면 img_data에 넣어줌
 
         imgNames = []
 
         for i, img in enumerate(img_data):
             url = bigImageUrl(img['data-source'])
             filetype = url.split(".")[-1]
+
+            if filetype != "jpg":
+                continue
+
             t = urlopen(url).read()
             filename = str(i+1) + "." + filetype
+
             with open(filename, "wb") as f:
                 f.write(t)
-            print("Img Save Success")
 
-            if filetype == "jpg":
-                imgNames.append(filename)
+            print(f"[{filename}]: Img Save Success")
+            imgNames.append(filename)
 
         print(imgNames)
         cropFace(imgNames)
